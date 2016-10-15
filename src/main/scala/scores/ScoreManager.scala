a8128618a1ad8dd805577e606fbd80db8ad01f1e
robert.makyla <robert.makyla@gmail.com>
2016-10-15 20:06:13 +0200

    score moanager -  de-duplicating

--- src/main/scala/scores/ScoreManager.scala
+++ src/main/scala/scores/ScoreManager.scala
@@ -3,16 +3,7 @@ package scores
 import questions.{QuestionAndAnswer, QuestionGenerator}
 import tokens._
 
-object Messages {
-  val testComplete = "Test is complete. Generate a new Token if you want to restart"
-  val finished = "You have finished"
-  val pass = "pass"
-  val noQuestion = "There's no pending question ..."
-  def broken(token: Token) = "Broken token: " + token.value
-}
-
 class ScoreManager(tokenGenerator: TokenGenerator, questionGenerator: QuestionGenerator, maxQuestions: Int) {
-  import Messages._
 
   private val scoreCards = collection.mutable.Map[Token, ScoreCard]()
 
@@ -22,32 +13,36 @@ class ScoreManager(tokenGenerator: TokenGenerator, questionGenerator: QuestionGe
     nextToken
   }
 
-  def nextQuestion(token: Token): String = scoreCards.get(token) match {
-    case Some(card) if card.isComplete => testComplete
-    case Some(ScoreCard(_, Some(q))) => q.question
-    case Some(card) =>
+  def nextQuestion(token: Token): String = withUnfinishedScoreCard(token) {
+    case ScoreCard(_, Some(q)) => q.question
+    case ScoreCard(score, None) =>
       val question = questionGenerator.next
-      scoreCards(token) = ScoreCard(card.score, Some(question))
+      scoreCards(token) = ScoreCard(score, Some(question))
       question.question
-    case None => broken(token)
   }
 
-  def answer(token: Token, answer: String): String = scoreCards.get(token) match {
-    case Some(card) if card.isComplete => testComplete
-    case Some(ScoreCard(_, None)) => noQuestion
-    case Some(ScoreCard(score, Some(question))) =>
+  def answer(token: Token, answer: String): String = withUnfinishedScoreCard(token) {
+    case ScoreCard(_, None) => "There's no pending question ..."
+    case ScoreCard(score, Some(question)) =>
       if (isCorrect(question, answer)) {
         val newCard = ScoreCard(score + 1, None)
         scoreCards(token) = newCard
-        if (newCard.isComplete) finished else pass
+        if (newCard.isComplete) "You have finished" else "pass"
       } else "fail"
-    case None => broken(token)
   }
+
+  private def withUnfinishedScoreCard(token: Token)(f: ScoreCard => String) =
+    scoreCards.get(token) match {
+      case Some(card) if card.isComplete => "Test is complete. Generate a new Token if you want to restart"
+      case Some(card) => f(card)
+      case None => "Broken token: " + token.value
+    }
 
   private def isCorrect(question: QuestionAndAnswer, answer: String) = answer == s"${question.expectedAnswer}"
 
   case class ScoreCard(score: Int = 0, question: Option[QuestionAndAnswer] = None) {
     def isComplete = score >= maxQuestions
   }
+
 }
 
