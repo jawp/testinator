11e1084b8fb0f3f4ef29ac337331f577c5ac30d2
robert.makyla <robert.makyla@gmail.com>
2016-10-15 16:07:18 +0200

    score manager -  refactoring [WIP]

--- src/main/scala/scores/ScoreManager.scala
+++ src/main/scala/scores/ScoreManager.scala
@@ -5,28 +5,32 @@ import tokens._
 
 class ScoreManager(tokenGenerator: TokenGenerator, maxQuestions: Int) {
 
-  private val scores = collection.mutable.Map[Token, Int]()
+  private val scoreCards = collection.mutable.Map[Token, ScoreCard]()
 
   def nextToken(name: String): Token = {
     val nextToken = tokenGenerator.nextToken
-    scores.getOrElseUpdate(nextToken, 0)
+    scoreCards.getOrElseUpdate(nextToken, ScoreCard())
     nextToken
   }
 
-  def nextQuestion(token: Token) = scores.get(token) match {
-    case Some(score) => s"what is ${score} ?"
+  def nextQuestion(token: Token) = scoreCards.get(token) match {
+    case Some(card) => s"what is ${card.score} ?"
     case None => "Broken token: " + token
   }
 
-  def answer(token: Token, answer: String) = scores.get(token) match {
-    case Some(score) =>
+  def answer(token: Token, answer: String) = scoreCards.get(token) match {
+    case Some(card) =>
       if (isCorrect(token, answer)) {
-        scores(token) += 1
-        if (isFinished(token)) "You have finished" else "pass"
+        scoreCards(token) = ScoreCard(card.score + 1, card.question)
+        if (isFinished(card)) "You have finished" else "pass"
       } else "fail"
     case None => "Broken token: " + token
   }
 
-  private def isCorrect(token: Token, answer: String) = answer == s"${scores(token)}"
-  private def isFinished(token: Token) = scores(token) >= maxQuestions
+  private def isCorrect(token: Token, answer: String) = answer == s"${scoreCards(token).score}"
+
+  private def isFinished(card: ScoreCard) = card.score + 1 >= maxQuestions
 }
+
+case class ScoreCard(score: Int = 0, question: Option[Question] = None)
+case class Question(value: String)
