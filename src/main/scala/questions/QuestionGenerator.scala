3113f38b0992b4c39f345825ce702a671ad49a76
robert.makyla <robert.makyla@gmail.com>
2016-10-19 22:19:25 +0200

    question generator - add random operators to RealQuestionGenerator

--- src/main/scala/questions/QuestionGenerator.scala
+++ src/main/scala/questions/QuestionGenerator.scala
@@ -7,13 +7,27 @@ class QuestionGenerator {
   def next: QuestionAndAnswer = {
     val n1 = nextInt
     val n2 = nextInt
-    val plus = "+"
-    QuestionAndAnswer("What is " + n1 + " " + plus + " " + n1 + "?", n1 + n2)
+    val op = randomOp
+    QuestionAndAnswer("What is " + n1 + " " + op.name + " " + n1 + "?", op.compute(n1, n2))
   }
 
-  private def nextInt = Random.nextInt(100)
+  private def nextInt = Random.nextInt() % 9 + 1
+
+  private def randomOp: Operation = (Random.nextInt() % 3).abs match {
+    case 0 => Plus
+    case 1 => Minus
+    case 2 => Multiply
+  }
 }
 
 case class QuestionAndAnswer(question: String, expectedAnswer: Int) {
   def accepts(answer: String) = answer == s"$expectedAnswer"
 }
+
+case class Operation(name: String, compute: (Int, Int) => Int)
+
+object Plus extends Operation("plus", _ + _)
+
+object Minus extends Operation("minus", _ - _)
+
+object Multiply extends Operation("multiplied by", _ * _)
