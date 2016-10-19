3f7e55bc7436408e8c832af73100016f77477bc6
robert.makyla <robert.makyla@gmail.com>
2016-10-19 21:40:34 +0200

    score manager - reducing more boiler plate in specs

--- src/test/scala/scores/ScoreManagerSpec.scala
+++ src/test/scala/scores/ScoreManagerSpec.scala
@@ -13,83 +13,77 @@ class ScoreManagerSpec extends FreeSpec with MustMatchers with Mockito {
 
     "when question is generated" - {
 
-      "finish after all correct answers (regardless other tokens activities)" in new Fixture {
+      "pass with correct answer" in new Fixture {
         val token = nextToken(smith)
-        nextQuestion(token) mustBe "what is 1 + 1 ?"
-        answer(token, "2") mustBe "pass"
-
-        val otherToken = nextToken(johnson)
-        nextQuestion(otherToken) mustBe "what is 1 + 1 ?"
-        answer(otherToken, "anyWrongAnswer") mustBe "fail"
-
-        nextQuestion(token) mustBe "what is 1 + 1 ?"
-        answer(token, "2") mustBe "You have finished"
+        assertQuestionPass(token)
+      }
+      "finish with last correct answer" in new Fixture {
+        val token = nextToken(smith)
+        assertQuestionPass(token)
+        assertQuestionFinish(token)
+      }
+      "fail with bad answer" in new Fixture {
+        val token = nextToken(smith)
+        assertQuestionFail(token)
       }
 
       "don't generate other questions until first one is answered" in new Fixture {
         val token = nextToken(smith)
-        nextQuestion(token) mustBe "what is 1 + 1 ?"
-        nextQuestion(token) mustBe "You already got the question, but OK, once again: what is 1 + 1 ?"
-        nextQuestion(token) mustBe "You already got the question, but OK, once again: what is 1 + 1 ?"
-        answer(token, "2") mustBe "pass"
-
-        nextQuestion(token) mustBe "what is 1 + 1 ?"
+        nextQuestion(token) mustBe "what year is it ?"
+        nextQuestion(token) mustBe "You already got the question, but OK, once again: what year is it ?"
+        nextQuestion(token) mustBe "You already got the question, but OK, once again: what year is it ?"
+        answer(token, "2016") mustBe "pass"
       }
 
       "after pass" - {
         "question should expire. You should be able to generate a new one" in new Fixture {
           val token = nextToken(smith)
-          nextQuestion(token) mustBe "what is 1 + 1 ?"
-          answer(token, "2") mustBe "pass"
-
+          assertQuestionPass(token)
           answer(token, "any") mustBe "There's no pending question ..."
-
-          nextQuestion(token) mustBe "what is 1 + 1 ?"
-          answer(token, "2") mustBe "You have finished"
+          assertQuestionFinish(token)
         }
       }
 
       "after fail" - {
         "don't accept answers or generate questions. Only a new token makes restart possible" in new Fixture {
           val token = nextToken(smith)
-          nextQuestion(token) mustBe "what is 1 + 1 ?"
-          answer(token, "wrongAnswer") mustBe "fail"
-
+          assertQuestionFail(token)
           nextQuestion(token) mustBe "Token is spoilt. Please generate a new token to restart."
           answer(token, "any") mustBe "Token is spoilt. Please generate a new token to restart."
-
-          assertRestartTestWithNewTokenFor(smith)
+          assertCompleteTestWithNewTokenFor(smith)
         }
       }
 
       "after finish" - {
         "don't accept answers or generate questions. Only a new token makes restart possible" in new Fixture {
-          val token = nextToken(smith)
-          nextQuestion(token) mustBe "what is 1 + 1 ?"
-          answer(token, "2") mustBe "pass"
-
-          nextQuestion(token) mustBe "what is 1 + 1 ?"
-          answer(token, "2") mustBe "You have finished"
-
+          val token = assertCompleteTestWithNewTokenFor(smith)
           answer(token, "any") mustBe "Test is complete. Please generate a new token if you want to restart"
           nextQuestion(token) mustBe "Test is complete. Please generate a new token if you want to restart"
-
-          assertRestartTestWithNewTokenFor(smith)
+          assertCompleteTestWithNewTokenFor(smith)
         }
       }
 
-      "after re-generating token" - {
-        "generate and return the new one, but also keep the old one" in new Fixture {
+      "after generating token for a new person" - {
+        "let the first person continue the test" in new Fixture {
           val token = nextToken(smith)
-          nextQuestion(token) mustBe "what is 1 + 1 ?"
-          answer(token, "2") mustBe "pass"
+          assertQuestionPass(token)
+
+          val otherToken = nextToken(johnson)
+          assertQuestionFail(otherToken)
+
+          assertQuestionFinish(token)
+        }
+      }
+
+      "after re-generating token for the same person" - {
+        "generate and return the new one, but also keep the old token" in new Fixture {
+          val token = nextToken(smith)
+          assertQuestionPass(token)
 
           val anotherToken = nextToken(smith)
-          nextQuestion(anotherToken) mustBe "what is 1 + 1 ?"
-          answer(anotherToken, "2") mustBe "pass"
+          assertQuestionPass(anotherToken)
 
-          nextQuestion(token) mustBe "what is 1 + 1 ?"
-          answer(token, "2") mustBe "You have finished"
+          assertQuestionFinish(token)
         }
       }
     }
@@ -113,33 +107,48 @@ class ScoreManagerSpec extends FreeSpec with MustMatchers with Mockito {
       answer(badToken, "any") mustBe s"Broken token: ${badToken.value}"
     }
 
-    "generate next token each time" in new Fixture {
+    "generate next token each time, even for the same person" in new Fixture {
       nextToken(smith) mustBe Token("0")
       nextToken(smith) mustBe Token("1")
-      nextToken(smith) mustBe Token("2")
+      nextToken(johnson) mustBe Token("2")
+      nextToken(johnson) mustBe Token("3")
     }
   }
 
 
   class Fixture {
     val questionGenerator = mock[QuestionGenerator]
-    questionGenerator.next returns QuestionAndAnswer("what is 1 + 1 ?", 2)
+    questionGenerator.next returns QuestionAndAnswer("what year is it ?", 2016)
 
     val scoreManager = new ScoreManager(new IncrementingTokenGenerator, questionGenerator, 2)
+
+    def assertCompleteTestWithNewTokenFor(name: String) = {
+      val token = nextToken(name)
+      assertQuestionPass(token)
+      assertQuestionFinish(token)
+      token
+    }
+
+    def assertQuestionPass(token: Token) = {
+      nextQuestion(token) mustBe "what year is it ?"
+      answer(token, "2016") mustBe "pass"
+    }
+
+    def assertQuestionFinish(token: Token) = {
+      nextQuestion(token) mustBe "what year is it ?"
+      answer(token, "2016") mustBe "You have finished"
+    }
+
+    def assertQuestionFail(token: Token) = {
+      nextQuestion(token) mustBe "what year is it ?"
+      answer(token, "wrongAnswer") mustBe "fail"
+    }
 
     def nextToken = scoreManager.nextToken _
 
     def nextQuestion = scoreManager.nextQuestion _
 
     def answer = scoreManager.answer _
-
-    def assertRestartTestWithNewTokenFor(name: String) = {
-      val newToken = nextToken(name)
-      nextQuestion(newToken) mustBe "what is 1 + 1 ?"
-      answer(newToken, "2") mustBe "pass"
-      nextQuestion(newToken) mustBe "what is 1 + 1 ?"
-      answer(newToken, "2") mustBe "You have finished"
-    }
   }
 
   class IncrementingTokenGenerator extends TokenGenerator {
