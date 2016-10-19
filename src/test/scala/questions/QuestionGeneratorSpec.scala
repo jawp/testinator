b48cccb7850ed23408ab5b6d8b2a19a18bdc2960
robert.makyla <robert.makyla@gmail.com>
2016-10-19 22:19:25 +0200

    question generator - add random operators to RealQuestionGenerator

--- src/test/scala/questions/QuestionGeneratorSpec.scala
+++ src/test/scala/questions/QuestionGeneratorSpec.scala
@@ -7,12 +7,23 @@ class QuestionGeneratorSpec extends FreeSpec with MustMatchers {
   "QuestionGenerator generates" - {
     "complex questions" in new Fixture {
       val qa = questionGenerator.next
-      qa.question.contains(" + ") mustBe true
       qa.question.contains(s"${qa.expectedAnswer}") mustBe false
+    }
+
+    "question with one of the three operators: plus, minus, multiplied by:" in new Fixture {
+      val qa = questionGenerator.next
+      List(Plus.name, Minus.name, Multiply.name).count(qa.question.contains) mustBe 1
+    }
+
+    "Operations make sense" in {
+      Plus.compute(-2, 10) mustBe 8
+      Minus.compute(-2, 10) mustBe -12
+      Multiply.compute(-2, 10) mustBe -20
     }
   }
 
   class Fixture {
     val questionGenerator = new QuestionGenerator
   }
+
 }
